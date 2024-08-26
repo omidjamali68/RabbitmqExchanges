387555069f113471e09d410e377ffb69c8dbf853
ojama <o.jamali2016@gmail.com>
2024-08-26 22:59:53 +0330

    Init Direct exchange

---
+++ BasketService/Program.cs
@@ -0,0 +1,16 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("order.create", true, false, false, null);
+string message = $"Send shopping card basket to order service {DateTime.UtcNow}";
+var body = Encoding.UTF8.GetBytes(message);
+
+var properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+// exchange:"" => Direct exchange
+channel.BasicPublish(exchange:"", routingKey:"order.create", basicProperties: properties, body: body);
---
+++ OrderService/Program.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+    string message = Encoding.UTF8.GetString(args.Body.ToArray());
+    Console.WriteLine("Recieved Message= " + message);
+    channel.BasicAck(args.DeliveryTag, true); 
+};
+
+channel.BasicConsume("order.create", false, consumer);
+Console.ReadLine();
